9d9495411d2eec1d9591b55a67c007deec7ace14
Nicole Torres <nicole.torres@mscoding.org>
2024-05-21 13:46:22 -0500

    Update Kiosk Project 1.AssemblyInfo.cs

--- Kiosk Project/Kiosk Project 1/obj/Debug/net8.0/Kiosk Project 1.AssemblyInfo.cs
+++ Kiosk Project/Kiosk Project 1/obj/Debug/net8.0/Kiosk Project 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kiosk Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e1f7068ffb6c49d56fff240f8e0e3c6ab3c4e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaa3540374a5aa934085440bc564777760c76cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kiosk Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kiosk Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
