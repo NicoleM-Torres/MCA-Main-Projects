a3416cfbfc4f130ed5afc8d1a957573cbf548a0a
Nicole Torres <nicole.torres@mscoding.org>
2024-05-21 13:46:26 -0500

    Create MoneyReq.cs

---
+++ Kiosk Project/Kiosk Project 1/MoneyReq.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk_Project_1
+{
+    internal class MoneyReq
+    {
+        #region MoneyRequest
+        public static string[] MoneyRequest(string account_number, decimal amount)
+        {
+
+            Random rnd = new Random();
+            //50% chance that transaction passes or fails
+            bool pass = rnd.Next(100) < 50;
+            //50% chance that a failed transaction is declined
+            bool declined = rnd.Next(100) < 50;
+
+            if (pass)
+            {
+                return new string[] { account_number, amount.ToString() };
+            }
+            else
+            {
+                if (!declined)
+                {
+                    return new string[] { account_number, (amount / rnd.Next(2, 6)).ToString() };
+                }
+                else
+                {
+                    return new string[] { account_number, "declined" };
+                }
+            }
+        }
+        #endregion
+
+    }
+}
