351cae2864dd03e4bf9ffc0c1a120b31c2656d39
Nicole Torres <nicole.torres@mscoding.org>
2024-05-21 14:36:30 -0500

    Create CashPayCheck.cs

---
+++ Kiosk Project/Kiosk Project 1/CashPayCheck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk_Project_1
+{
+    internal class CashPayCheck ()
+    {
+        #region CashPay
+        public static void CashPay(decimal totalCost)
+        {
+            CashChange.CalculateChange(totalCost);
+            //call the function to get the total change
+            decimal totalChange = CashChange.CalculateChange(totalCost);
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Your total change is ${0}", totalChange);
+
+            //call the function to check the bank
+            bool payGood = Bank.CheckBankCard(totalChange);
+
+            //if the bank is ok then dispense change
+            if (payGood == true) CashChange.dispenseChange(totalChange);
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Thank you for using a NHS Kiosk");
+
+        } //END CASHPAY METHOD
+        #endregion 
+
+        #region CheckDollarBills
+        public static int CheckBill(decimal numbercheck)
+        {
+            if (numbercheck == 100) return 0;
+            else if (numbercheck == 50) return 1;
+            else if (numbercheck == 20) return 2;
+            else if (numbercheck == 10) return 3;
+            else if (numbercheck == 5) return 4;
+            else if (numbercheck == 2) return 5;
+            else if (numbercheck == 1) return 6;
+            else if (numbercheck == .50m) return 7;
+            else if (numbercheck == .25m) return 8;
+            else if (numbercheck == .10m) return 9;
+            else if (numbercheck == .05m) return 10;
+            else if (numbercheck == .01m) return 11;
+            else return 12;
+        }
+#endregion
+
+
+    }//END CLASS CASHPAYMENTS
+}
