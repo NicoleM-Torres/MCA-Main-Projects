d1e3f64343fda0389ce32cb8ad46435a768cbb80
Nicole Torres <nicole.torres@mscoding.org>
2024-05-21 13:46:11 -0500

    Create RecieptLog.cs

---
+++ Kiosk Project/Kiosk Project 1/RecieptLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Kiosk_Project_1
+{
+    internal class RecieptLog
+    {
+
+        #region Transaction LogIn
+        public static void transactionLogging()
+        {
+            string vendor = Kiosk.cardVendor.Replace(' ', '`');
+            string arguments = Kiosk.transactionNum.ToString() + "," + Kiosk.date + ",$" + Kiosk.cashAmount.ToString() + "," + vendor + ",$" + Kiosk.cardAmount.ToString() + ",$" + Kiosk.changeDue.ToString();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "notepad.exe";
+            startInfo.Arguments = arguments;
+            Process.Start(startInfo);
+        }
+        #endregion
+    
+
+    }
+}
