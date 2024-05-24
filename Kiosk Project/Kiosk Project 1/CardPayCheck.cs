7aba97cce672411f70315fa77c4ad936376d16c3
Nicole Torres <nicole.torres@mscoding.org>
2024-05-24 15:10:18 -0500

    Update CardPayCheck.cs

--- Kiosk Project/Kiosk Project 1/CardPayCheck.cs
+++ Kiosk Project/Kiosk Project 1/CardPayCheck.cs
@@ -22,13 +22,13 @@ namespace Kiosk_Project_1
             decimal newTotal = 0;
             bool getCashBack = true;
 
-            bool realNumCheck = true;
+            bool bankHasMoney = true;
             decimal numberCheck = 0;
 
             //Validates if it is the actual length of a card
             do
             {
-                creditCard = Prompt("Please enter a card number");         
+                creditCard = Prompt("Enter a card number:");         
                 if (creditCard.Length != 16) Console.WriteLine("(Please enter a valid card number)");
             } while (creditCard.Length != 16);
 
@@ -37,7 +37,7 @@ namespace Kiosk_Project_1
             Kiosk.cardVendor = vendor;
 
             //if it is a good vendor it then checks to make sure its a valid card
-            if (vendor != "Non valid card")
+            if (vendor != "Invalid Card -- TRY AGAIN")
             {
                 isGood = CardCheckVal.ValidateCard(creditCard);
 
@@ -47,10 +47,10 @@ namespace Kiosk_Project_1
                     Console.WriteLine("***************************************");
                     do
                     {
-                        Console.Write("Card was invalid, would you like to enter a new one?(y/n):");
+                        Console.Write("Card was invalid, would you like to enter a new one? (y/n):");
                         answer = Console.ReadLine();
                         answer = answer.ToLower().Substring(0, 1);
-                        if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                        if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                     } while (answer != "y" && answer != "n");
                     if (answer == "y") return -3;
                 }
@@ -61,7 +61,7 @@ namespace Kiosk_Project_1
                 Console.WriteLine("***************************************");
                 do
                 {
-                    Console.Write("Card was invalid, would you like to enter a new one?(y/n):");
+                    Console.Write("ERROR \n Card was invalid, would you like to enter a new one?(y/n):");
                     answer = Console.ReadLine();
                     answer = answer.ToLower().Substring(0, 1);
                     if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
@@ -79,7 +79,7 @@ namespace Kiosk_Project_1
                         Console.Write("Would you like to pay with cash instead (y/n):");
                         answer = Console.ReadLine();
                         answer = answer.ToLower().Substring(0, 1);
-                        if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                        if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                     } while (answer != "y" && answer != "n");
 
                     //if its yes then it sends them to the cash payment
@@ -104,7 +104,7 @@ namespace Kiosk_Project_1
                     Console.Write("Would you like to pay with cash instead? (y/n):");
                     answer = Console.ReadLine();
                     answer = answer.ToLower().Substring(0, 1);
-                    if (answer != "y" && answer != "n") Console.WriteLine("(Please enter 'y' or 'n':)");
+                    if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n':)");
                 } while (answer != "y" && answer != "n");
 
                 //if its yes then it sends them to the cash payment
@@ -120,10 +120,10 @@ namespace Kiosk_Project_1
                 Console.WriteLine("***************************************");
                 do
                 {
-                    Console.Write("Do you want Cash back?(y/n)");
+                    Console.Write("Do you want Cash back? (y/n)");
                     answer = Console.ReadLine();
                     answer = answer.ToLower().Substring(0, 1);
-                    if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                    if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                 } while (answer != "y" && answer != "n");
                 //if yes it sends them to the cashback function
                 if (answer == "y")
@@ -140,41 +140,44 @@ namespace Kiosk_Project_1
                     }
                     else if (cashBack < 0)
                     {
-                        Console.WriteLine("This machine does not have the funds required to give cash back");
+                        Console.WriteLine("We're sorry! :( \n The NHS Kiosk does not have sufficient funds to dispense the cashback requested.");
                         getCashBack = false;
                     }
                     //if they chose no cash back it prints this message
                 }
                 else
                 {
-                    Console.WriteLine("You chose no cash Back");
+                    Console.WriteLine("NO CASHBACK");
                     getCashBack = false;
                 }
 
                 //start checking the users bank to see if they have enough money to pay for it
                 string[] availability = MoneyReq.MoneyRequest(creditCard, totalCost);
-                string checkPayment = availability[1];
 
-                realNumCheck = decimal.TryParse(checkPayment, out numberCheck);
+                //string checkPayment = availability[1];
+                //bankHasMoney = decimal.TryParse(checkPayment, out numberCheck);
 
-                realNumCheck = true;
+                bankHasMoney = true;
                 numberCheck = totalCost;
+
                 //check to see if the payment went through
-                if (realNumCheck == true)
+                if (bankHasMoney == true)
                 {
+
                     //if it went through congratulations
                     if (numberCheck == totalCost)
                     {
                         Console.WriteLine("***************************************");
-                        if (getCashBack == true) Console.WriteLine("Congrats your payment went through and {0:C} was dispensed to you", cashBack);
-                        else Console.WriteLine("Congrats your payment went through");
+                        if (getCashBack == true) Console.WriteLine("Your payment was APPROVED and {0:C} cashback was dispensed.", cashBack);
+                        else Console.WriteLine("Your payment was APPROVED.");
                         Kiosk.cardAmount = totalCost + cashBack;
                         Kiosk.changeDue = cashBack;
                         return -1;
                         //else if your card only can only pay for part of it then you ask if they want to pay the rest with a new card
-                    }
-                    else
-                    {
+                    } 
+                        
+                    else {
+
                         Console.WriteLine("***************************************");
                         do
                         {
@@ -190,10 +193,10 @@ namespace Kiosk_Project_1
 
                             numberCheck = decimal.Parse(toString);
 
-                            Console.Write("Your Card Could only pay {0:C} Would you like to enter a new card? ", numberCheck);
+                            Console.Write("Your card vendor only approved {0:C} towards your amount due. Would you like to enter a new card? ", numberCheck);
                             answer = Console.ReadLine();
                             answer = answer.ToLower().Substring(0, 1);
-                            if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                            if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                         } while (answer != "y" && answer != "n");
                         // if yes it returns what they have left to pay
                         if (answer == "y") return numberCheck * -1;
@@ -203,10 +206,10 @@ namespace Kiosk_Project_1
                         {
                             do
                             {
-                                Console.Write("Would you like to pay with cash instead (y/n):");
+                                Console.Write("Would you like to pay with cash instead? (y/n):");
                                 answer = Console.ReadLine();
                                 answer = answer.ToLower().Substring(0, 1);
-                                if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                                if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                             } while (answer != "y" && answer != "n");
 
                             //if yes it returns the amount and goes to cash
@@ -223,10 +226,10 @@ namespace Kiosk_Project_1
                     Console.WriteLine("***************************************");
                     do
                     {
-                        Console.Write("your card was declined because you didnt have enough in your bank would you like to try another card?");
+                        Console.Write("Card DECLINED -- Insufficient funds.\n Would you like to try another card?");
                         answer = Console.ReadLine();
                         answer = answer.ToLower().Substring(0, 1);
-                        if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                        if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                     } while (answer != "y" && answer != "n");
 
                     //if yes it sends them back to the start of the project
@@ -241,7 +244,7 @@ namespace Kiosk_Project_1
                             Console.Write("Would you like to pay with cash instead (y/n):");
                             answer = Console.ReadLine();
                             answer = answer.ToLower().Substring(0, 1);
-                            if (answer != "y" && answer != "n") Console.WriteLine("(please enter y or n)");
+                            if (answer != "y" && answer != "n") Console.WriteLine("(Enter 'y' or 'n')");
                         } while (answer != "y" && answer != "n");
 
                         //if yes it sends them to the cash payment
