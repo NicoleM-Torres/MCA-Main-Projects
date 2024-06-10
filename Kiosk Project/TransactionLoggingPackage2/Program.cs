0771029504b900ee09a9b840a1685e38a80a7437
Nicole Torres <nicole.torres@mscoding.org>
2024-06-10 12:38:12 -0500

    Update Program.cs

--- Kiosk Project/TransactionLoggingPackage2/Program.cs
+++ Kiosk Project/TransactionLoggingPackage2/Program.cs
@@ -9,16 +9,23 @@ namespace TransactionLoggingPackage2
 
 
         {
-            string vendor = trans.cardVendor.Replace(' ', '_');
-            string output = trans.transactionNum.ToString() + "," + trans.transactionDate + ",$" + trans.cashAmount.ToString()
-            + "," + vendor + ",$" + trans.cardAmount + ",$" + trans.changeGiven.ToString();
-            var dateOnly = DateTime.Now.ToString("MM-dd-yyyy");
+            
+            //string vendor = trans.cardVendor.Replace(' ', '_');
+            //string output = trans.transactionNum.ToString() + "," + trans.transactionDate + ",$" + trans.cashAmount.ToString()
+            //+ "," + vendor + ",$" + trans.cardAmount + ",$" + trans.changeGiven.ToString();
+            //var dateOnly = DateTime.Now.ToString("MM-dd-yyyy");
+
+            string transactionNumber = args[0];
+            string transDate = args[1];
+            string transCashAmount = args[2];
+            string vendor = args[3];
+            string cardAmount = args[4];
+            string changeGiven = args[5];
 
             //create log file folder
             string folder = @"C:\Users\nicol\Documents\GitHub\MCA-Main-Projects\Kiosk Project\TransactionLoggingPackage2";
-            string receiptLogFile = dateOnly;
-            string filepath =
-            string fileName = dateOnly;
+            string receiptLogFile = transDate;
+            string filepath = @"C:\Users\nicol\Documents\GitHub\MCA-Main-Projects\Kiosk Project\TransactionLoggingPackage2\bin\Debug\net8.0\TransactionLoggingPackage2.exe";
 
 
             if (!File.Exists(filepath))
@@ -29,7 +36,7 @@ namespace TransactionLoggingPackage2
 
                 {
                     //Write text to file
-                    for (int i = 0; i < output.Length; i++) { sw.WriteLine(output[i]); }
+                    for (int i = 0; i < args.Length; i++) { sw.WriteLine(args[i]); }
                     //Close the file
                     sw.Close();
                 }
@@ -38,14 +45,11 @@ namespace TransactionLoggingPackage2
             using (StreamWriter sw = File.AppendText(filepath))
 
             {
-                for (int i = 0; i < output.Length; i++) { sw.WriteLine(output[i]); }
+                for (int i = 0; i < args.Length; i++) { sw.WriteLine(args[i]); }
                 //Close the file
                 sw.Close();
 
             }
-
-
-
         }//end main
 
         #region PROMPT FUNCTIONS
