e16c2bfa0cfd697121bdad1c59c66caac551dcb4
Nicole Torres <nicole.torres@mscoding.org>
2024-06-10 15:03:33 -0500

    Update Kiosk Final 2.0.AssemblyInfo.cs

--- Kiosk Project/Kiosk Final 2.0/obj/Debug/net8.0/Kiosk Final 2.0.AssemblyInfo.cs
+++ Kiosk Project/Kiosk Final 2.0/obj/Debug/net8.0/Kiosk Final 2.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kiosk Final 2.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fa45632c06d9286e16430b4f53238fed0f7a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5bbff92cc9c3141d93c820d76bdfff84bdc6f12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kiosk Final 2.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kiosk Final 2.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
